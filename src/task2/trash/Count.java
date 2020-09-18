b7bd6fd78372503a9de1f3388528eccd90645e5d
dima <test@sd.com>
2020-09-18 18:47:13 +0500

    2. JCIP. Настройка библиотеки [#333754]

---
+++ src/task2/trash/Count.java
@@ -0,0 +1,18 @@
+package task2.trash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public void increment() {
+        this.value++;
+    }
+
+    public int get() {
+        return this.value;
+    }
+}
