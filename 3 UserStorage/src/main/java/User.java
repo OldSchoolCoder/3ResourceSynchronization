985048baa58a7c7c40382bfbcd4ffead96763430
dima <test@sd.com>
2020-10-10 21:29:08 +0500

    3. Класс хранилища пользователей UserStorage [#333756]

--- 3 UserStorage/src/main/java/User.java
+++ 3 UserStorage/src/main/java/User.java
@@ -1,9 +1,40 @@
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.Objects;
+
 public class User {
-    int id;
-    int amount;
+    private final int id;
+    private int amount;
 
     public User(int id, int amount) {
         this.id = id;
         this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                amount == user.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, amount);
     }
 }
--- 3 UserStorage/src/main/java/UserStore.java
+++ 3 UserStorage/src/main/java/UserStore.java
@@ -1,20 +1,33 @@
+import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ThreadSafe
 public class UserStore {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
 
     public synchronized void transfer(int fromId, int toId, int amount) {
+        int oldAmount;
+        oldAmount = users.get(fromId).getAmount();
+        users.get(fromId).setAmount(oldAmount - amount);
+        oldAmount = users.get(toId).getAmount();
+        users.get(toId).setAmount(oldAmount + amount);
     }
 
     public synchronized boolean add(User user) {
-        return false;
+        users.put(id.incrementAndGet(), user);
+        return users.containsValue(user);
     }
 
     public synchronized boolean update(User user) {
-        return false;
+        users.put(user.getId(), user);
+        return users.containsValue(user);
     }
 
     public synchronized boolean delete(User user) {
-        return false;
+        return users.remove(user.getId(), user);
     }
 }
